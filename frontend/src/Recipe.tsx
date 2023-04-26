d58162c4abacaf10cf85c5783c3a977da7d3628c
ErinNPeters <erin.nicole.peters@gmail.com>
2023-04-26 16:44:05 -0500

    Link from home page to view recipe works

--- frontend/src/Recipe.tsx
+++ frontend/src/Recipe.tsx
@@ -12,56 +12,56 @@ interface Props {
 }
 
 export const Recipe = ({ data, showContent = true }: Props) => (
-  <RecipeBox>
-    <div
-      css={css`
-        padding: 10px 0px;
-      `}
-    >
+  <Link
+    css={css`
+      text-decoration: none;
+      color: ${gray2};
+    `}
+    to={`/recipes/${data.recipeId}`}
+  >
+    <RecipeBox>
       <div
         css={css`
           padding: 10px 0px;
-          font-size: 19px;
         `}
       >
-        {/* <Link
-        css={css`
-          text-decoration: none;
-          color: ${gray2};
-        `}
-        to={`/questions/${data.questionId}`}
-      > */}
-        {data.title}
-        {/* </Link> */}
-      </div>
-      {showContent && (
         <div
           css={css`
-            padding-bottom: 10px;
-            font-size: 15px;
-            color: ${gray2};
+            padding: 10px 0px;
+            font-size: 19px;
           `}
         >
-          {data.ingredients.slice(0, 5).map((ingredient) => (
-            <div key={ingredient.ingredientId}>
-              <React.Fragment>
-                {new Fraction(ingredient.amount).toFraction().toString()}{' '}
-                {ingredient.unitOfMeasure} {ingredient.name}
-              </React.Fragment>
-            </div>
-          ))}
-          ...
+          {data.title}
         </div>
-      )}
-      <div
-        css={css`
-          font-size: 12px;
-          font-style: italic;
-          color: ${gray3};
-        `}
-      >{`Added by ${
-        data.userName
-      } on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()} `}</div>
-    </div>
-  </RecipeBox>
+        {showContent && (
+          <div
+            css={css`
+              padding-bottom: 10px;
+              font-size: 15px;
+              color: ${gray2};
+            `}
+          >
+            {data.ingredients.slice(0, 5).map((ingredient) => (
+              <div key={ingredient.ingredientId}>
+                <React.Fragment>
+                  {new Fraction(ingredient.amount).toFraction().toString()}{' '}
+                  {ingredient.unitOfMeasure} {ingredient.name}
+                </React.Fragment>
+              </div>
+            ))}
+            ...
+          </div>
+        )}
+        <div
+          css={css`
+            font-size: 12px;
+            font-style: italic;
+            color: ${gray3};
+          `}
+        >{`Added by ${
+          data.userName
+        } on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()} `}</div>
+      </div>
+    </RecipeBox>
+  </Link>
 );
